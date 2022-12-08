e7b7773cd80f61f2749a044be8c32edd8575802d
Bala Ganeshan <bala.ganeshan@outlook.com>
2022-12-07 22:40:05 -0800

    finishing exercise

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
 
-	fmt.Println("Math")
+	i1, i2, i3 := 12, 45, 68
 
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer Sum is ", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Floating Sum is ", floatSum)
+
+	floatSum = math.Round(floatSum*100)/100
+	fmt.Println("The floating sum is now ", floatSum)
 }
