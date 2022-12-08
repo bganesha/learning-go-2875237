6abb34f7c5963d2d7005db01e0965d14e1f1cbea
Bala Ganeshan <bala.ganeshan@outlook.com>
2022-12-07 22:52:37 -0800

    Finishing exercise

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Println("I ran this program at ", n)
 
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Println("Go launched at ", t)
+
+	fmt.Println(t.Format(time.ANSIC))
 }
